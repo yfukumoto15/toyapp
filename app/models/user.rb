eae15bd64d96ffc5a76bca613bf2b9dbe5107325
yfukumoto15 <yfukumoto@netprotections.co.jp>
2025-04-26 03:55:21 +0000

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :microposts
+  validates （コードを書き込む）, presence: true    # （コードを書き込む）の中身を書き換えてください
+  validates （コードを書き込む）, presence: true    # （コードを書き込む）の中身を書き換えてください
+end
